fe61bb29b91057cfe20af13d23998148ec4d4e0b
Jaseela99 <jaseelachittur@gmail.com>
2022-09-05 18:27:20 +0530

    modified

---
+++ ecommerce/src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "./style.css"
+export default function Nav() {
+    return <div className="nav-container">
+        <ul className="nav-list">
+        <li>Home</li>
+        <li>Products</li>
+        <li>Signin</li>
+        </ul>
+        </div>
+}
